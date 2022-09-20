ca1b15ab5c1dea08f39d05126ab92972960a268c
Sunnyzer <nassim.ziouiche@gmail.com>
2022-09-20 19:38:07 +0200

    Add code Interact

--- DBDGameplay/Source/DBDGameplay/DBD_Survivor.cpp
+++ DBDGameplay/Source/DBDGameplay/DBD_Survivor.cpp
@@ -1,5 +1,48 @@
 #include "DBD_Survivor.h"
 
+#include "Interactable.h"
+#include "Kismet/KismetSystemLibrary.h"
+
 ADBD_Survivor::ADBD_Survivor()
 {	
+}
+
+
+void ADBD_Survivor::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Interact",EInputEvent::IE_Pressed, this, &ADBD_Survivor::Interact);
+	PlayerInputComponent->BindAction("Interact",EInputEvent::IE_Released, this, &ADBD_Survivor::StopInteract);
+	PlayerInputComponent->BindAxis("MoveForward", this, &ADBD_Survivor::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ADBD_Survivor::MoveRight);
+}
+
+void ADBD_Survivor::Interact()
+{
+	TArray<AActor*> _actorToIgnore;
+	FHitResult _hitResult;
+	bool _hit = UKismetSystemLibrary::SphereTraceSingleForObjects(this, GetActorLocation(),GetActorLocation(), radiusInteract,objectTypeInteract,false,_actorToIgnore, drawDebug,_hitResult,true);
+	if(_hit)
+	{
+		IInteractable* _interactableObject = Cast<IInteractable>(_hitResult.GetActor());
+		if(_interactableObject)
+			_interactableObject->Interactable(this);
+	}
+}
+
+void ADBD_Survivor::StopInteract()
+{
+		
+}
+
+void ADBD_Survivor::MoveForward(float _axis)
+{
+	if(_axis != 0)
+		AddMovementInput(GetActorForwardVector(), _axis);
+}
+
+void ADBD_Survivor::MoveRight(float _axis)
+{
+	if(_axis != 0)
+		AddMovementInput(GetActorRightVector(), _axis);
 }
--- DBDGameplay/Source/DBDGameplay/DBD_Survivor.h
+++ DBDGameplay/Source/DBDGameplay/DBD_Survivor.h
@@ -6,19 +6,36 @@
 #include "DBD_Character.h"
 #include "DBD_Survivor.generated.h"
 
-/**
- * 
- */
+namespace EDrawDebugTrace
+{
+	enum Type;
+}
+
 UCLASS()
 class DBDGAMEPLAY_API ADBD_Survivor : public ADBD_Character
 {
 	GENERATED_BODY()
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float repareSpeed = 1;
+		float repareSpeed = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float radiusInteract = 100;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<TEnumAsByte<EObjectTypeQuery>> objectTypeInteract;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TEnumAsByte<EDrawDebugTrace::Type> drawDebug;
 public:
 	ADBD_Survivor();
 public:
 	float GetRepareSpeedValue() const { return repareSpeed; }
-	
+protected:
+	UFUNCTION(BlueprintCallable)
+		void Interact();
+	UFUNCTION(BlueprintCallable)
+		void StopInteract();
+	UFUNCTION(BlueprintCallable)
+		void MoveForward(float _axis); 
+	UFUNCTION(BlueprintCallable)
+		void MoveRight(float _axis); 
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 };
